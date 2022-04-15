41104fa54fc11582e765f4f2adcf278e2147d837
Chinmay <shalawadi.chinmay@gmail.com>
2022-04-15 06:12:03 -0600

    external interrupt done

--- main.c
+++ main.c
@@ -4,12 +4,22 @@
 #include "joystick.h"
 
 void init_uart();
+void init_interrupt();
+// void pca_interrupt() __interrupt(3);
 
 #define UART_CR2_TEN (1 << 3)
 #define UART_CR3_STOP2 (1 << 5)
 #define UART_CR3_STOP1 (1 << 4)
 #define UART_SR_TXE (1 << 7)
 
+const char *a[5];
+
+// ------------------------------------------------init-uart------------------------------------------------
+/***********************************************************************************
+ * function : Shows the eeprom menu and waits for user input
+ * parameters : none
+ * return : none
+ ***********************************************************************************/
 void init_uart()
 {
     //Pin D6 Tx, D5 Rx
@@ -22,7 +32,32 @@ void init_uart()
     UART1_BRR2 = 0x0B;
     UART1_BRR1 = 0x08; // 9600 baud
 }
+// ------------------------------------------------init-interrupt------------------------------------------------
+/***********************************************************************************
+ * function : Shows the eeprom menu and waits for user input
+ * parameters : none
+ * return : none
+ ***********************************************************************************/
+void init_interrupt()
+{
+    PA_DDR = 0x0;
+    PA_CR1 = 0x8;
+    PA_CR2 = 0x8;
+    EXTI_CR1 |= 0x2;
+}
 
+void pca_interrupt(void) __interrupt(3)
+{
+    //A3    
+    printf("In Interrupt3 \n\r");
+}
+
+// ------------------------------------------------putchar-------------------------------------------------
+/***********************************************************************************
+ * function : Shows the eeprom menu and waits for user input
+ * parameters : none
+ * return : none
+ ***********************************************************************************/
 int putchar(int c)
 {
     while (!(UART1_SR & UART_SR_TXE))
@@ -32,10 +67,16 @@ int putchar(int c)
 
     return (c);
 }
-
+// ------------------------------------------------main-------------------------------------------------
+/***********************************************************************************
+ * function : Shows the eeprom menu and waits for user input
+ * parameters : none
+ * return : none
+ ***********************************************************************************/
 void main(void)
 {
-    const char *a[5];
+
+    __asm__("rim");
 
     a[0] = "IDLE";
     a[1] = "UP";
@@ -48,6 +89,7 @@ void main(void)
     unsigned long i = 0;
     start_adc();
     start_adc();
+    init_interrupt();
     for (;;)
     {
         printf("%s\n", a[get_joystick_pos()]);
@@ -56,3 +98,4 @@ void main(void)
             ; // Sleep
     }
 }
+// ------------------------------------------------End-------------------------------------------------
--- regdef.h
+++ regdef.h
@@ -38,4 +38,36 @@
 #define ADC_AWCRH 	(*(volatile uint8_t *)0x540E)
 #define ADC_AWCRL 	(*(volatile uint8_t *)0x540F)
 
+#define PA_ODR   	(*(volatile uint8_t *)0x5000)
+#define PA_IDR   	(*(volatile uint8_t *)0x5001)
+#define PA_DDR   	(*(volatile uint8_t *)0x5002)
+#define PA_CR1   	(*(volatile uint8_t *)0x5003)
+#define PA_CR2   	(*(volatile uint8_t *)0x5004)
+
+#define PB_ODR   	(*(volatile uint8_t *)0x5005)
+#define PB_IDR   	(*(volatile uint8_t *)0x5006)
+#define PB_DDR   	(*(volatile uint8_t *)0x5007)
+#define PB_CR1   	(*(volatile uint8_t *)0x5008)
+#define PB_CR2   	(*(volatile uint8_t *)0x5009)
+
+#define PC_ODR   	(*(volatile uint8_t *)0x500A)
+#define PC_IDR   	(*(volatile uint8_t *)0x500B)
+#define PC_DDR   	(*(volatile uint8_t *)0x500C)
+#define PC_CR1   	(*(volatile uint8_t *)0x500D)
+#define PC_CR2   	(*(volatile uint8_t *)0x500E)
+
+#define PD_ODR   	(*(volatile uint8_t *)0x500F)
+#define PD_IDR   	(*(volatile uint8_t *)0x5010)
+#define PD_DDR   	(*(volatile uint8_t *)0x5011)
+#define PD_CR1   	(*(volatile uint8_t *)0x5012)
+#define PD_CR2   	(*(volatile uint8_t *)0x5013)
+
+#define EXTI_CR1   	(*(volatile uint8_t *)0x50A0)
+#define EXTI_CR2   	(*(volatile uint8_t *)0x50A1)
+
+#define CCR        	(*(volatile uint8_t *)0x7F0A)
+#define CFG_GCR    	(*(volatile uint8_t *)0x7F60)
+
+
+
 #endif
