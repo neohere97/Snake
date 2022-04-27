830cfdf7cc1c5434d8cbc9b2ad747f09e818be92
Chinmay Shalawadi <shalawadi.chinmay@gmail.com>
2022-04-27 12:22:18 -0600

    Update beeper_motor.h

--- beeper_motor.h
+++ beeper_motor.h
@@ -1,6 +1,16 @@
+/***************************************************************************
+ * ESD Final Project, Spring 2022
+ * Tools: VSCode,make,stm8flash,SDCC
+ * Author: Chinmay Shalawadi
+ * Institution: University of Colorado Boulder
+ * Mail id: chsh1552@colorado.edu
+ * References: SDCC Documentation, STM8 datasheet & Reference Manuals
+ ***************************************************************************/
+
 #ifndef BEEPER_H
 #define BEEPER_H
 
+//function declarations, please see the c file for function descriptions
 void init_beeper();
 void enable_beeper();
 void disable_beeper();
