5940ddf8f304a1019a7cdd463c98793ddfb097f9
Chinmay Shalawadi <shalawadi.chinmay@gmail.com>
2022-04-27 12:22:52 -0600

    Update display.h

--- display.h
+++ display.h
@@ -1,6 +1,16 @@
+/***************************************************************************
+ * ESD Final Project, Spring 2022
+ * Tools: VSCode,make,stm8flash,SDCC
+ * Author: Chinmay Shalawadi
+ * Institution: University of Colorado Boulder
+ * Mail id: chsh1552@colorado.edu
+ * References: SDCC Documentation, MAX7219 Datasheet
+ ***************************************************************************/
+
 #ifndef DISPLAY_
 #define DISPLAY_
 
+//function declarations, please see the c file for function descriptions
 void init_display();
 void clear_display();
 
