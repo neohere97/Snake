cde2ce98a10e2b736a77673f99bff6b0105b1868
Chinmay <shalawadi.chinmay@gmail.com>
2022-04-28 06:17:37 -0600

    last code comment

--- uart.h
+++ uart.h
@@ -1,6 +1,16 @@
+/***************************************************************************
+ * ESD Final Project, Spring 2022
+ * Tools: VSCode,make,stmflash,SDCC
+ * Author: Chinmay Shalawadi
+ * Institution: University of Colorado Boulder
+ * Mail id: chsh1552@colorado.edu
+ * References: SDCC Documentation & STM8 Manuals
+ ***************************************************************************/
+
 #ifndef UART_H_
 #define UART_H_
 
+//function declarations
 void init_uart();
 
 #endif
